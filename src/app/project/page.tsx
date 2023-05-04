bf2425acaf0f6057e5939832f064349812467a6e
malvinju <malvinjuanda@yahoo.com>
2023-05-04 22:32:27 +0700

    Change card bg color

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function ProjectPage() {
     return(
         <section>
-            <div className="max-w-2xl px-6 py-6 mx-auto bg-white rounded-xl border-2 dark:border dark:bg-gray-800 dark:border-gray-600">
+            <div className="max-w-2xl px-6 py-6 mx-auto bg-white rounded-xl border-2 dark:border dark:bg-neutral-900 dark:border-gray-600">
                 <Image className="rounded-lg" src={projectMovingObjcet} alt="" />
                 <div className="flex items-center justify-between mt-3">
                     <span className="text-sm font-light text-gray-600 dark:text-gray-400">2018</span> 
@@ -30,7 +30,7 @@ export default function ProjectPage() {
                 </div> 
             </div>
 
-            <div className="max-w-2xl px-6 py-6 mx-auto  mt-5 bg-white rounded-xl border-2 dark:border dark:bg-gray-800 dark:border-gray-600">
+            <div className="max-w-2xl px-6 py-6 mx-auto  mt-5 bg-white rounded-xl border-2 dark:border dark:bg-neutral-900 dark:border-gray-600">
                 <Image className="rounded-lg" src={projectEastco} alt="" />
                 <div className="flex items-center justify-between mt-3">
                     <span className="text-sm font-light text-gray-600 dark:text-gray-400">2017</span> 
